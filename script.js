728332b0a5129b103d43d633d7c2198bcd88d65e
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-01-29 13:31:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required
 document.querySelector('input[type="button"]').addEventListener('click',(e)=>{
 	let scolor = document.getElementById('colorSelect')
+	if(scolor.selectedIndex !== -1)
 	scolor.remove(scolor.value)
 })
